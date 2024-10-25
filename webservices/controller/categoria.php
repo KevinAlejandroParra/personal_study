792601226710e6ea5c378cb898f04a3280d328a1
alejandrocifuent <luisparra5380@gmail.com>
2024-10-24 22:49:42 -0500

    feature: CRUD with API HTTP

---
+++ webservices/controller/categoria.php
@@ -0,0 +1,43 @@
+<?php
+header('Content-type: application/json');
+require_once("../config/conexion.php");
+require_once("../models/categoria.php");
+
+$categoria = new categoria();
+
+$body = json_decode(file_get_contents("php://input"), true);
+
+switch($_GET["op"]){
+ 
+
+    case "GetAll";
+    $datos=$categoria->get_categoria();
+    echo json_encode($datos);
+    break;
+
+    case "GetId";
+    $datos=$categoria->get_categoria_x_id($body["cat_id"]);
+    echo json_encode($datos);
+    break;
+
+    case "insert";
+    $datos=$categoria->insert_categoria($body["cat_nom"], $body ["cat_obs"]);
+    echo json_encode("insert correcto");
+    break;
+
+    case "Update";
+    $datos=$categoria->update_categoria($body["cat_id"], $body ["cat_nom"], $body["cat_obs"]);
+    echo json_encode("Update correcto");
+    break;
+
+    case "Delete";
+    $datos=$categoria->delete_categoria($body["cat_id"]);
+    echo json_encode("inactivo");
+    break;
+
+    case "Eliminar";
+    $datos=$categoria->eliminar_categoria($body["cat_id"]);
+    echo json_encode("Borrada correctamente");
+    break;
+}
+?>  
